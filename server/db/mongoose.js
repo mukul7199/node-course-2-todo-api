e6115dd3e60dc939ebbeddd5abae33a540459ace
Mukul lalwani <mukul7199@gmail.com>
2018-11-15 13:56:45 +0530

    setup different test and dev envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 const keys = require('./keys');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const express = require('express');
 const _ = require('lodash');
 const bodyParser = require('body-parser');
@@ -85,7 +87,7 @@ app.patch('/todos/:id', (req, res) => {
         if(!todo) {
             return res.status(404).send();
         }
-        
+
         res.send({todo});
     }).catch(e => {
         res.status(400).send();
