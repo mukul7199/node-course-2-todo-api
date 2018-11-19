af10a94fa0644ceeb4d5e55b1571a98bc977b074
Mukul lalwani <mukul7199@gmail.com>
2018-11-19 19:35:47 +0530

    Add post/user

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,48 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
-let User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        trim: true
-    }
+        trim: true,
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+UserSchema.methods.generateAuthToken = function() {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access, token}]);
+    
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+let User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
